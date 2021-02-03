dd13eba9debbbe6041de494bf18c3e09daad4651
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-02-03 17:20:17 +0100

    fix: Graceful Error Handling

--- api/users/index.ts
+++ api/users/index.ts
@@ -1,5 +1,5 @@
 import { NowRequest, NowResponse } from "@vercel/node";
-import { isValidAddress, toChecksumAddress } from "ethereumjs-util";
+import { toChecksumAddress } from "ethereumjs-util";
 import { getModel } from "../../utils/mongo";
 
 export default async (req: NowRequest, res: NowResponse): Promise<NowResponse | void> => {
@@ -9,7 +9,9 @@ export default async (req: NowRequest, res: NowResponse): Promise<NowResponse | 
 
   let { address } = req.query;
   address = address as string;
-  if (isValidAddress(address)) {
+
+  // Sanity check for address; to avoid any SQL-like injections, ...
+  if (address.match(/^0x[0-9a-fA-F]{40}$/)) {
     const userModel = await getModel("User");
     const user = await userModel.findOne({ address: address.toLowerCase() }).lean();
     if (!user) {
