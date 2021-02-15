80cc09b53d36441b5ed6749651d8a5825defee9e
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-02-15 15:34:56 +0100

    fix: Undefined behavior

--- __tests__/message.test.ts
+++ __tests__/message.test.ts
@@ -4,7 +4,7 @@ describe("UTILS - verifyMessage", () => {
   it("cannot parse address from empty signature", async () => {
     const address = await verifyMessage("CakeIsBae", "");
 
-    expect(address).not.toBeDefined();
+    expect(address).toStrictEqual(undefined);
   });
 
   it("cannot parse address from invalid signature", async () => {
@@ -13,6 +13,6 @@ describe("UTILS - verifyMessage", () => {
       "8144a6fa26be252b86456491fbcd43c1de7e022241845ffea1c3df066f7cfede"
     );
 
-    expect(address).not.toBeDefined();
+    expect(address).toStrictEqual(undefined);
   });
 });
