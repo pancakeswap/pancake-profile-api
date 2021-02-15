6d148c349f9c5d1c4d80836d22c59bf9e22b352b
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-02-15 15:55:39 +0100

    test: More verifyMessage tests

--- __tests__/message.test.ts
+++ __tests__/message.test.ts
@@ -1,6 +1,15 @@
 import { verifyMessage } from "../utils";
 
 describe("UTILS - verifyMessage", () => {
+  it("can parse address from valid signature", async () => {
+    const address = await verifyMessage(
+      "CakeIsBae",
+      "0x033fc8f01625a76c1423f70e274ce91369250eb660642704c70e06535ada0d8d6af8f84dc611732e8a318fbb08ec0ade9027dacdc2e67971b5f56fe4b18ed7751b"
+    );
+
+    expect(address).toStrictEqual("0xed54a7c1d8634bb589f24bb7f05a5554b36f9618");
+  });
+
   it("cannot parse address from empty signature", async () => {
     const address = await verifyMessage("CakeIsBae", "");
 
