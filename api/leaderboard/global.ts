154359e6b4989d1bc44a72cc3fb52695b9f01f76
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-05-27 21:31:27 +0200

    chore(leaderboard): Refactor global logic

--- api/leaderboard/global.ts
+++ api/leaderboard/global.ts
@@ -3,16 +3,29 @@ import { toChecksumAddress } from "ethereumjs-util";
 import { gql, request } from "graphql-request";
 import { TRADING_COMPETITION_V1_SUBGRAPH } from "../../utils";
 
+interface User {
+  id: string;
+  volumeUSD: string;
+  team: {
+    id: string;
+  };
+}
+
 export default async (req: VercelRequest, res: VercelResponse): Promise<VercelResponse | void> => {
   if (req.method?.toUpperCase() === "OPTIONS") {
     return res.status(204).end();
   }
 
-  const results = await request(
+  const result = await request(
     TRADING_COMPETITION_V1_SUBGRAPH,
     gql`
       {
-        users(first: 500, block: { number: 6553043 }, orderBy: volumeUSD, orderDirection: desc) {
+        competition(id: "1") {
+          id
+          userCount
+          volumeUSD
+        }
+        users(first: 500, orderBy: volumeUSD, orderDirection: desc, block: { number: 6553043 }) {
           id
           volumeUSD
           team {
@@ -23,16 +36,16 @@ export default async (req: VercelRequest, res: VercelResponse): Promise<VercelRe
     `
   );
 
-  const volume = results.users.reduce((acc: any, user: any) => {
-    return acc + parseFloat(user.volumeUSD);
-  }, 0);
-
-  const data = results.users.map((user: any, index: number) => ({
+  const data = result.users.map((user: User, index: number) => ({
     rank: index + 1,
     address: toChecksumAddress(user.id),
     volume: parseFloat(user.volumeUSD),
     teamId: parseInt(user.team.id),
   }));
 
-  return res.status(200).json({ total: results.users.length, volume: volume, data });
+  return res.status(200).json({
+    total: parseInt(result.competition.userCount),
+    volume: parseFloat(result.competition.volumeUSD),
+    data,
+  });
 };
--- api/leaderboard/team.ts
+++ api/leaderboard/team.ts
@@ -3,6 +3,14 @@ import { toChecksumAddress } from "ethereumjs-util";
 import { gql, request } from "graphql-request";
 import { TRADING_COMPETITION_V1_SUBGRAPH } from "../../utils";
 
+interface User {
+  id: string;
+  volumeUSD: string;
+  team: {
+    id: string;
+  };
+}
+
 export default async (req: VercelRequest, res: VercelResponse): Promise<VercelResponse | void> => {
   if (req.method?.toUpperCase() === "OPTIONS") {
     return res.status(204).end();
@@ -12,33 +20,36 @@ export default async (req: VercelRequest, res: VercelResponse): Promise<VercelRe
   teamId = teamId as string;
 
   if (teamId) {
-    const results = await request(
+    const result = await request(
       TRADING_COMPETITION_V1_SUBGRAPH,
       gql`
         {
-            users(first: 500, where: { team: "${teamId}" }, block: { number: 6553043 }, orderBy: volumeUSD, orderDirection: desc) {
+            users (first: 500, where: { team: "${teamId}" }, orderBy: volumeUSD, orderDirection: desc, block: { number: 6553043 }) {
                 id
                 volumeUSD
                 team {
                     id
+                    userCount
                 }
             }
         }
     `
     );
 
-    const volume = results.users.reduce((acc: any, user: any) => {
+    const volume = result.users.reduce((acc: number, user: User) => {
       return acc + parseFloat(user.volumeUSD);
     }, 0);
 
-    const data = results.users.map((user: any, index: number) => ({
+    const data = result.users.map((user: User, index: number) => ({
       rank: index + 1,
       address: toChecksumAddress(user.id),
       volume: parseFloat(user.volumeUSD),
       teamId: parseInt(user.team.id),
     }));
 
-    return res.status(200).json({ total: results.users.length, volume: volume, data });
+    return res
+      .status(200)
+      .json({ total: parseInt(result.users[0].team.userCount), volume: volume, data });
   }
 
   return res.status(400).json({ error: { message: "Team unknown." } });
